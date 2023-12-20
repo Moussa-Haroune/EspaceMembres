85bbe424f154bac3191c774b87f4829a1539a6a2
Moussa Haroune <haroune.hmah@gmail.com>
2023-12-20 12:10:47 +0100

    ajout du projet

---
+++ src/forgot.php
@@ -0,0 +1,41 @@
+<?php
+    require_once __DIR__.'/../config/config.php';
+
+    if(isset($_POST['email']) && !empty($_POST['email']))
+    {
+        $email = htmlspecialchars($_POST['email']);
+
+        // check pour verifier si l'utilisateur existe !
+        $check = $bdd->prepare('SELECT token FROM utilisateurs WHERE email = ?');
+        $check->execute(array($email));
+        $data = $check->fetch();
+        $row = $check->rowCount();   // nombre de ligne que va nous renvoyer le requete sql
+
+        // si row == 0 c'est à dire l'utilisateur n'est pas dans notre p
+        if($row)
+        {
+            $token = bin2hex(openssl_random_pseudo_bytes(24));
+            $token_user = $data['token'];
+
+            $insert = $bdd->prepare('INSERT INTO password_recover(token_user, token) VALUES (?,?)');
+            $insert->execute(array($token_user, $token));
+
+            $link = 'recover.php?u='.base64_encode($token_user).'&token='.base64_encode($token);
+            echo "<a href='$link'>lien</a>";
+            /*
+            // Envoyer le lien par e-mail
+            $to = $email;
+            $subject = 'Récupération de mot de passe';
+            $message = "Cliquez sur le lien suivant pour réinitialiser votre mot de passe : $link";
+            $headers = 'From: votre@email.com'; // Remplacez cela par votre adresse e-mail
+
+            // Utiliser la fonction mail() pour envoyer l'e-mail
+            mail($to, $subject, $message, $headers);
+
+            echo "Un lien de réinitialisation a été envoyé à votre adresse e-mail.";
+
+            */
+        } else{
+            echo "Compte non existant";
+        }
+    }
