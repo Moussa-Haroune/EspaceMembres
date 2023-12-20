f35ab9ca89e530be98f2793464b8b32a168bfeda
Moussa Haroune <haroune.hmah@gmail.com>
2023-12-20 13:48:47 +0100

    titre change

--- index.php
+++ index.php
@@ -14,7 +14,7 @@
           <div class="col-11">
               <div class="card text-center m-4 shadow p-3 mb-5 bg-white rounded">
                 <div class="card-body">
-                  <h4 class="card-title p-3">J'ai oublié mon mot de passe</h4>
+                  <h4 class="card-title p-3"> mot de passe oublié !</h4>
                     <div class="form-group">
                         <form action="src/forgot.php" method="POST">
                             <input type="email" class="form-control" name="email" placeholder="Email" autocomplete="off" required />
