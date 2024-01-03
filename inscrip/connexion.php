69545f92cbf22b823d4fc41504790d76dd5778d6
Moussa Haroune <haroune.hmah@gmail.com>
2024-01-03 18:35:36 +0100

    inscription data traitement update config file

---
+++ inscrip/connexion.php
@@ -0,0 +1,35 @@
+<?php
+    session_start();           // Démarrage de la session
+    require_once(__DIR__.'../config.php');
+
+    // Si il existe les champs email, password et qu'il sont pas vident
+
+    if(isset($_POST['email']) && isset($_POST['password']))  
+    {
+        $email = htmlspecialchars($_POST['email']); 
+        $password = htmlspecialchars($_POST['password']);
+        $check = $bdd->prepare('SELECT pseudo, email, password, token FROM utilisateurs WHERE email = ?');
+        $check->execute(array($email));
+        $data = $check->fetch();
+        $row = $check->rowCount();
+        
+        if($row == 1)
+        {
+            if(filter_var($email, FILTER_VALIDATE_EMAIL))
+            {
+                $password = hash('sha256', $password);
+                if($data['password'] === $password)
+                {
+                    $_SESSION['user'] = $data['pseudo'];
+                    header('Location:landing.php');
+                    die();
+
+                }else{ header('Location:index.php?login_err=password'); die(); }
+
+            }{header('Location:index.php?login_err=email'); die();}
+
+        }else{ header('Location:index.php?login_err=already'); die();}
+
+    }else {header('Location:index.php'); die();}
+    
+?>
